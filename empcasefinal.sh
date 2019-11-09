fe73f884eb75b1c34f59d12811d253f1248b9550
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-09 18:17:38 -0500

    comments added

--- empcasefinal.sh
+++ empcasefinal.sh
@@ -4,7 +4,7 @@ reminder=$(( $random % 3 ))
 RatePerHour=10
 ftime=1
 ptime=2
-
+#here is my comment to changes
 case $reminder in
 	$fulltime )
 	echo "employee fulltime found" ;;
@@ -12,5 +12,5 @@ case $reminder in
 	echo "employee parttime found" ;;
 	* )
 	echo "employee Not found" ;;
-	
+#here is my comment to changes
 esac
