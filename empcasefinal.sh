753e9074c8a7ae94c9e42961231b86ea5189dd93
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-09 17:40:17 -0500

    part time branch

--- empcasefinal.sh
+++ empcasefinal.sh
@@ -4,6 +4,7 @@ reminder=$(( $random % 3 ))
 RatePerHour=10
 fulltime=1
 parttime=2
+
 case $reminder in
 	$fulltime )
 	hours=8 ;;
@@ -12,5 +13,6 @@ case $reminder in
 	* )
 	hours=0 ;;
 esac
+
 average=$(( $RatePerHour * $hours ))
 echo $average
