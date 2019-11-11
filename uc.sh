5cf841918fc64bab56612a5dffc8ffd443f6e381
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-11 18:01:53 -0500

    uc6

--- uc.sh
+++ uc.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -x
 
+# store the result in a dictionary for every computation.
+
+declare -A calculations
+
 echo -e "enter the number :\c"
 read a b c 
 echo "numbers are : $a $b $c"
@@ -13,3 +17,11 @@ echo $result1
 echo $result2
 echo $result3
 echo $result4
+
+calculations[result1]="$result1"
+calculations[result2]="$result2"
+calculations[result3]="$result3"
+calculations[result4]="$result4"
+
+echo "key : ${!calculations[@]} value : ${calculations[@]}"
+
