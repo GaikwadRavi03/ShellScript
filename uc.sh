cc6937a0f0247fa792d979d693a5e3c35195f4cd
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-11 17:31:18 -0500

    uc-3

--- uc.sh
+++ uc.sh
@@ -4,5 +4,8 @@ echo -e "enter the number :\c"
 read a b c 
 echo "numbers are : $a $b $c"
 
-result=$(($a+$b*$c))
-echo $result
+result1=$(($a+$b*$c))
+result2=$(($a*$b+$c))
+
+echo $result1
+echo $result2
