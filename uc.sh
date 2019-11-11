c150dff9e260573516cbfed579d5410f5de8bf80
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-11 16:45:38 -0500

    merge uc  2

--- uc.sh
+++ uc.sh
@@ -3,8 +3,6 @@
 echo -e "enter the numbers :\c"
 read a b c 
 echo "numbers are : $a $b $c"
-<<<<<<< HEAD
-=======
 result=$(($a+$b*$c))
 echo $result
->>>>>>> uc2
+
--- uc3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-
-echo -e "enter the numbers :\c"
-read a b c 
-echo "numbers are : $a $b $c"
-result=$(($a*$b+$c))
-echo $result
