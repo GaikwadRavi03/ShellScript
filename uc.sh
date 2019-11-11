23b7d8b25b12e629776a1222451e13ba20b669f7
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-11 17:44:30 -0500

    uc-5

--- uc.sh
+++ uc.sh
@@ -7,7 +7,9 @@ echo "numbers are : $a $b $c"
 result1=$(($a+$b*$c))
 result2=$(($a*$b+$c))
 result3=$(($c+$a/$b))
+result4=$(($a%$b+$c))
 
 echo $result1
 echo $result2
 echo $result3
+echo $result4
